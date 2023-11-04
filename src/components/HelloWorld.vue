050316992ef72c75d95ece2ec8f5edff72be0248
alexandervikenfalk <alexander.vikenfalk@homeday.de>
2023-11-04 16:55:40 +0100

    🌐 add internationalization.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: number }>()
 
 const count = ref(0)
+
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  {{ $t("home.header") }}
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
@@ -20,7 +21,7 @@ const count = ref(0)
   <p>
     Check out
     <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
+    >create-vue</a
     >, the official Vue + Vite starter
   </p>
   <p>
