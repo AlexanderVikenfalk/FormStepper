28a8d569199ce563a52b07eb4cb6b7d623d75bdc
alexandervikenfalk <alexander.vikenfalk@homeday.de>
2023-11-05 12:14:11 +0100

    ✨ add generic SVG component.

---
+++ src/components/shared/icon.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { defineAsyncComponent } from 'vue';
+
+const props = defineProps<{
+  name: string
+}>()
+
+// Vite doesn't support these kind of dynamic imports. Error is ignored until a fix is found.
+const icon = defineAsyncComponent(() =>
+    import(`/src/assets/${props.name}.svg`/* @vite-ignore */)
+);
+</script>
+
+<template>
+  <component :is="icon" />
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import i18n from "./i18n"
+import router from './router/index';
+import './index.css'
+import icon from '@/components/shared/icon.vue';
 
-createApp(App).use(i18n).mount('#app')
+createApp(App)
+    .use(i18n)
+    .use(router)
+    .component('icon', icon)
+    .mount('#app')
