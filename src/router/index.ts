f8fb1822c0ab761f57c19040d7fbd74d161732c2
alexandervikenfalk <alexander.vikenfalk@homeday.de>
2023-11-06 20:20:21 +0100

    💩 add temp routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,35 +1,36 @@
-import {RouteRecordRaw, createRouter, createWebHistory } from 'vue-router'
-import DefaultLayout from '@/layouts/DefaultLayout.vue'
+import { RouteRecordRaw, createRouter, createWebHistory } from "vue-router";
+import DefaultLayout from "@/layouts/DefaultLayout.vue";
+import WizardLayout from "@/layouts/WizardLayout.vue";
 
 const routes: Array<RouteRecordRaw> = [
-    {
-        name: 'Home',
-        path: '/home',
-        component: () => import('../views/Stepper.vue'),
-        meta: {layout: DefaultLayout}
-    },
-    {
-        name: 'Stepper',
-        path: '/stepper',
-        component: () => import('../views/Stepper.vue'),
-        meta: {layout: DefaultLayout}
-    },
-    {
-        name: 'Redirect',
-        path: '/',
-        redirect: '/home',
-    },
-    {
-        name: 'NotFound',
-        path: '/:pathMatch(.*)*',
-        component: () => import('../views/NotFound.vue'),
-        meta: {layout: DefaultLayout}
-    },
-]
+  {
+    name: "Home",
+    path: "/home",
+    component: () => import("../views/Stepper.vue"),
+    meta: { layout: WizardLayout },
+  },
+  {
+    name: "Stepper",
+    path: "/stepper",
+    component: () => import("../views/Stepper.vue"),
+    meta: { layout: WizardLayout },
+  },
+  {
+    name: "Redirect",
+    path: "/",
+    redirect: "/home",
+  },
+  {
+    name: "NotFound",
+    path: "/:pathMatch(.*)*",
+    component: () => import("../views/NotFound.vue"),
+    meta: { layout: DefaultLayout },
+  },
+];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
-})
+  history: createWebHistory(),
+  routes,
+});
 
-export default router
+export default router;
