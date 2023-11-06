f1e578f76b55a13ab2f6e74afefb0f8ada0f06d9
alexandervikenfalk <alexander.vikenfalk@homeday.de>
2023-11-06 17:26:44 +0100

    🎨 add default layout.

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bg-white w-4/5 h-[80vh] shadow-md flex flex-col" >
+    <header class="w-full border-b">
+
+    </header>
+    <main class=" flex flex-row p-8 flex-auto">
+      <slot/>
+    </main>
+<!--    <TheFooter />-->
+  </div>
+</template>
+
+<script setup>
+import TheStepper from "@/components/TheStepper.vue";
+import TheFooter from "@/components/TheFooter.vue";
+</script>
