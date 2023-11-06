23364fb810a8d9a674a091605c2b1f304fe0f54c
alexandervikenfalk <alexander.vikenfalk@homeday.de>
2023-11-06 17:31:23 +0100

    🎨 update code to support multiple language imports.

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,38 +1,16 @@
 import { createI18n } from "vue-i18n";
-
-const messages      = {
-    en: {
-        nav: {
-            home: "Home",
-            about: "About"
-        },
-        home: {
-            header: "Welcome to the Vue 3 I18n tutorial!",
-            created_by: "This tutorial was brought to you by Lokalise."
-        },
-        about: {
-            header: "About us"
-        }
-    },
-    se: {
-        nav: {
-            home: "Hem",
-            about: "Om"
-        },
-        home: {
-            header: "Välkommen till Vue 3 guiden!",
-            created_by: "Denna guide skapades av",
-        },
-        about: {
-            header: "Om oss"
-        }
-    }
-}
+import en from "./locales/en.json"
+import de from "./locales/de.json"
+import se from "./locales/se.json"
 
 export default createI18n({
     locale: import.meta.env.VITE_DEFAULT_LOCALE,
     fallbackLocale: import.meta.env.VITE_FALLBACK_LOCALE,
     legacy: false,
     globalInjection: true,
-    messages
+    messages : {
+        en,
+        de,
+        se,
+    }
 })
