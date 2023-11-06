d21230772c768896f9e7fcf73abcb92a5beae497
alexandervikenfalk <alexander.vikenfalk@homeday.de>
2023-11-06 23:18:33 +0100

    🎨 make component safer.

--- src/components/shared/icon.vue
+++ src/components/shared/icon.vue
@@ -1,16 +1,17 @@
 <script setup lang="ts">
-import { defineAsyncComponent } from 'vue';
+import { defineAsyncComponent } from "vue";
 
+// When importing a new icon the name has to be updated here.
 const props = defineProps<{
-  name: string
-}>()
+  name: "logo" | "nameData" | "terms" | "welcome";
+}>();
 
 // Vite doesn't support these kind of dynamic imports. Error is ignored until a fix is found.
-const icon = defineAsyncComponent(() =>
-    import(`/src/assets/${props.name}.svg`/* @vite-ignore */)
+const icon = defineAsyncComponent(
+  () => import(`/src/assets/${props.name}.svg` /* @vite-ignore */),
 );
 </script>
 
 <template>
-  <component :is="icon" />
+  <component :is="icon || DefaultIcon" />
 </template>
