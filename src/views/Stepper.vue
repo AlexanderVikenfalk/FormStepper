55ec0b1b625ecfd54194435602d2f3575783cc36
alexandervikenfalk <alexander.vikenfalk@homeday.de>
2023-11-06 20:21:08 +0100

    🎨 update stepper structure.

--- src/views/Stepper.vue
+++ src/views/Stepper.vue
@@ -1,24 +1,24 @@
 <script setup lang="ts">
-import { Field, ErrorMessage } from 'vee-validate';
-import * as yup from 'yup';
-import FormWizard from '@/components/FormWizard.vue';
-import FormStep from '@/components/FormStep.vue';
+import { Field, ErrorMessage } from "vee-validate";
+import * as yup from "yup";
+import FormWizard from "@/components/FormWizard.vue";
+import FormStep from "@/components/FormStep.vue";
 
 // break down the validation steps into multiple schemas
 const validationSchema = [
   yup.object({
-    firstName: yup.string().required().label('First Name'),
-    lastName: yup.string().required().label('Last Name'),
-    userName: yup.string().required().label('Github User Name'),
+    firstName: yup.string().required().label("First Name"),
+    lastName: yup.string().required().label("Last Name"),
+    userName: yup.string().required().label("Github User Name"),
   }),
   yup.object({
-    email: yup.string().required().email().label('Email'),
+    email: yup.string().required().email().label("Email"),
     terms: yup
-        .boolean()
-        .default(false)
-        .required()
-        .equals([true])
-        .label('Accept Terms and Conditions'),
+      .boolean()
+      .default(false)
+      .required()
+      .equals([true])
+      .label("Accept Terms and Conditions"),
   }),
 ];
 
@@ -31,34 +31,64 @@ function onSubmit(formData) {
 </script>
 
 <template>
-    <FormWizard :validation-schema="validationSchema" @submit="onSubmit">
+  <FormWizard :validation-schema="validationSchema" @submit="onSubmit">
+    <!--      // STEP 1-->
+    <FormStep>
+      <label class="wizard-input-label" for="firstName">{{
+        $t("wizard.first_name")
+      }}</label>
+      <Field
+        class="wizard-input"
+        name="firstName"
+        type="text"
+        placeholder="Axel"
+      />
+      <ErrorMessage class="wizard-input-error" name="firstName" />
 
-<!--      // STEP 1-->
-      <FormStep>
-        <label class="wizard-input-label" for="firstName">First name</label>
-        <Field class="wizard-input" name="firstName" type="text" placeholder="Axel" />
-        <ErrorMessage class="wizard-input-error" name="firstName" />
+      <label class="wizard-input-label" for="firstName">Last name</label>
+      <Field
+        class="wizard-input"
+        name="lastName"
+        type="text"
+        placeholder="Springer"
+      />
+      <ErrorMessage class="wizard-input-error" name="lastName" />
 
-        <label class="wizard-input-label" for="firstName">Last name</label>
-        <Field class="wizard-input" name="lastName" type="text" placeholder="Springer" />
-        <ErrorMessage class="wizard-input-error" name="lastName" />
+      <label class="wizard-input-label" for="firstName">
+        Github user name</label
+      >
+      <Field
+        class="wizard-input"
+        name="userName"
+        type="text"
+        placeholder="Caesar"
+      />
+      <ErrorMessage class="wizard-input-error" name="userName" />
+    </FormStep>
 
-        <label class="wizard-input-label" for="firstName"> Github user name</label>
-        <Field class="wizard-input" name="userName" type="text" placeholder="Caesar" />
-        <ErrorMessage class="wizard-input-error" name="userName" />
-      </FormStep>
+    <!--      // STEP 2-->
+    <FormStep>
+      <label class="wizard-input-label" for="email">Email</label>
+      <Field
+        class="wizard-input"
+        name="email"
+        type="text"
+        placeholder="Axel@springer.de"
+      />
+      <ErrorMessage class="wizard-input-error" name="email" />
 
-<!--      // STEP 2-->
-      <FormStep>
-        <label class="wizard-input-label" for="email">Email</label>
-        <Field class="wizard-input" name="email" type="text" placeholder="Axel@springer.de" />
-        <ErrorMessage class="wizard-input-error" name="email" />
-
-        <label class="wizard-input-label" for="terms">Agreeing to the terms and conditions</label>
-        <Field class="wizard-checkbox" name="terms" type="checkbox" :value="true" />
-        <ErrorMessage class="wizard-input-error" name="terms">test </ErrorMessage>
-      </FormStep>
-    </FormWizard>
+      <label class="wizard-input-label" for="terms"
+        >Agreeing to the terms and conditions</label
+      >
+      <Field
+        class="wizard-checkbox"
+        name="terms"
+        type="checkbox"
+        :value="true"
+      />
+      <ErrorMessage class="wizard-input-error" name="terms">test </ErrorMessage>
+    </FormStep>
+  </FormWizard>
 </template>
 
 <style>
