c7766b3cc66b1c825708ab5e058d71cc5950a6a6
alexandervikenfalk <alexander.vikenfalk@homeday.de>
2023-11-06 20:04:59 +0100

    ♻️ refactor translation stucture.

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,16 +1,23 @@
-import { createI18n } from "vue-i18n";
-import en from "./locales/en.json"
-import de from "./locales/de.json"
-import se from "./locales/se.json"
+import { createI18n, I18nOptions } from "vue-i18n";
+import pluralRules from "./rules/pluralization";
+import numberFormats from "./rules/numbers.js";
+import datetimeFormats from "./rules/datetime.js";
+import en from "./locales/en.json";
+import de from "./locales/de.json";
+import se from "./locales/se.json";
 
-export default createI18n({
-    locale: import.meta.env.VITE_DEFAULT_LOCALE,
-    fallbackLocale: import.meta.env.VITE_FALLBACK_LOCALE,
-    legacy: false,
-    globalInjection: true,
-    messages : {
-        en,
-        de,
-        se,
-    }
-})
+const i18nOptions: I18nOptions = {
+  locale: import.meta.env.VITE_DEFAULT_LOCALE as string,
+  fallbackLocale: import.meta.env.VITE_FALLBACK_LOCALE as string,
+  legacy: false,
+  globalInjection: true,
+  messages: { en, de, se },
+  runtimeOnly: false,
+  pluralRules,
+  numberFormats,
+  datetimeFormats,
+};
+
+const i18n = createI18n(i18nOptions);
+
+export default i18n;
