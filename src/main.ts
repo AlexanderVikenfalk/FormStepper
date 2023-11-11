99417fb1cc864f97ecf55e52d9eaf5983cc872ba
alexandervikenfalk <alexander.vikenfalk@homeday.de>
2023-11-11 13:19:27 +0100

    🔧 update config

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-import App from "./App.vue";
-import i18n from "@/i18n";
-import router from "./router/index";
-import "./index.css";
-import Icon from "@/components/shared/icon.vue";
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import i18n from '@/i18n'
+import router from './router/index'
+import './index.css'
+import Icon from '@/components/shared/iconPresenter.vue'
 
 createApp(App)
   .use(i18n)
   .use(router)
   .use(createPinia())
-  .component("Icon", Icon)
-  .mount("#app");
+  .component('Icon', Icon)
+  .mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import { dirname, resolve } from "node:path";
-import { fileURLToPath } from "url";
-import svgLoader from "vite-svg-loader";
-import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'url'
+import svgLoader from 'vite-svg-loader'
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import eslintPlugin from 'vite-plugin-eslint'
 
 export default defineConfig({
   plugins: [
@@ -13,17 +14,29 @@ export default defineConfig({
     VueI18nPlugin({
       include: resolve(
         dirname(fileURLToPath(import.meta.url)),
-        "./src/i18n/locales/**",
+        './src/i18n/locales/**',
       ),
     }),
     tsconfigPaths(),
+    eslintPlugin({
+      // add options for eslintPlugin here
+      include: [
+        './src/**/*.ts',
+        './src/**/*.d.ts',
+        './src/**/*.tsx',
+        './src/**/*.vue',
+      ], // specify files
+      emitWarning: true, // always emit errors as warnings, will not break the build
+      failOnWarning: false, // do not fail the build on warnings (default is false)
+      failOnError: false, // do not fail the build on errors (default is true in production)
+    }),
   ],
   resolve: {
     alias: [
       {
-        find: "@",
-        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+        find: '@',
+        replacement: fileURLToPath(new URL('./src', import.meta.url)),
       },
     ],
   },
-});
+})
